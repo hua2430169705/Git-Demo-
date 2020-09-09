a42b12f139e4b1a44ff45daee2d505ab4223e01d
hua2430169705 <2430169705@qq.com>
2020-09-09 16:55:46 +0800

    master update commit

--- demo-module-01/src/com/duyi/MainTest.java
+++ demo-module-01/src/com/duyi/MainTest.java
@@ -3,5 +3,7 @@ package com.duyi;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("watch file");
+
+        System.out.println("test branch update");
     }
 }
