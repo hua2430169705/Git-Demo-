43ba792328e241fe70e8d6593c53ddcb5df0e2fb
hua2430169705 <2430169705@qq.com>
2020-09-09 16:36:20 +0800

    master update

--- demo-module-01/src/com/duyi/MainTest.java
+++ demo-module-01/src/com/duyi/MainTest.java
@@ -3,5 +3,11 @@ package com.duyi;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("watch file");
+
+        System.out.println("master  update ");
+        System.out.println("master 中修改了代码~~");
     }
+
+
+
 }
